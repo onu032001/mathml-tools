28cbc46f1dcc58be5163c63f3899ad92b14c2a03
onu032001 <85744292+onu032001@users.noreply.github.com>
2025-11-07 14:23:46 +0900

    Almost done

--- mathml-parser.js
+++ mathml-parser.js
@@ -1,6 +1,6 @@
 /**
  * Parses MathML Element.
- * @param {MathMLElement} mmlElem MathML element parsed using MathML.
+ * @param {Element} mmlElem MathML element parsed using MathML.
  * @returns {string}
  */
 function MathMLElementToJS(mmlElem) {
@@ -11,29 +11,82 @@ function MathMLElementToJS(mmlElem) {
     const childName = child.nodeName.toLowerCase();
     switch (childName) {
       case 'mn':
-        result += child.nodeValue;
+        result += child.textContent;
         break;
       case 'mo':
-        result += ` ${child.nodeValue} `;
+        if (child.form === 'infix' || child.form === 'postfix') {
+          result += child.textContent;
+        } else if (child.separator === 'true') {
+          result += child.textContent + ' ';
+        } else {
+          result += ` ${child.textContent} `;
+        }
         break;
       case 'mi':
-        result += child.nodeValue;
+        if (Math[child.textContent]) {
+          result += 'Math.' + child.textContent;
+        } else if (child.textContent === 'π') {
+          result += 'Math.PI';
+        } else if (child.textContent === 'e') {
+          reslt += 'Math.E';
+        } else {
+          result += child.textContent;
+        }
         break;
-      case 'msup':
+      case 'msup': {
         let childNodesForSup = Array.from(child.childNodes);
-        const exponent = childNodesForSup.pop();
-        const base = childNodesForSup.pop();
-        result += `Math.pow(${base}, ${exponent})`;
+        const superscriptElem = childNodesForSup.pop();
+        const superscript = MathMLElementToJS(superscriptElem);
+        const baseElem = childNodesForSup.pop();
+        const base = MathMLElementToJS(baseElem);
+        result += `${base} ** ${superscript}`;
         break;
+      }
       case 'mrow':
+        result += MathMLElementToJS(child);
         break;
+      case 'msqrt': {
+        const sqrtInside = MathMLElementToJS(child);
+        result += `Math.sqrt(${sqrtInside})`;
+        break;
+      }
+      case 'mroot': {
+        let childNodesForRoot = Array.from(child.childNodes);
+        const indexElem = childNodesForRoot.pop();
+        const index = MathMLElementToJS(indexElem);
+        const baseElem = childNodesForRoot.pop();
+        const base = MathMLElementToJS(baseElem);
+        result += `${base} ** (1 / ${index})`;
+      }
       default:
         break;
     }
   });
+  return result;
 }
 function MathMLStringToJS(mml) {
   const parser = new DOMParser();
-  let mmlElem = parser.parseFromString(mml, 'text/xml');
+  let mmlElem = parser.parseFromString(mml, 'text/xml').documentElement;
+  const errorElement = mmlElem.querySelector('parsererror');
+  if (errorElement) {
+    throw new Error(errorElement.innerText);
+  }
   return MathMLElementToJS(mmlElem);
 }
+let MathMLString =
+`<math xmlns="http://www.w3.org/1998/Math/MathML">
+  <msqrt>
+    <mn>2</mn>
+  </msqrt>
+  <mo>+</mo>
+  <msqrt>
+    <mn>3</mn>
+  </msqrt>
+  <mo>+</mo>
+  <msup>
+    <mi>e</mi>
+    <mi>x</mi>
+  </msup>
+</math>`;
+let JSResult = MathMLStringToJS(MathMLString);
+console.log(JSResult);
